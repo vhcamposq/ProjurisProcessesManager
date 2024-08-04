c14b33569b3892d4b3e7a692cf7d1f22f6ec8439
Vinicius Campos <vhcamposquadros@hotmail.com>
2024-08-04 14:01:54 -0300

    Criando primeiras entidades.

---
+++ src/main/java/com/vhcamposq/projurisprocessesmanager/entity/Lawyer.java
@@ -0,0 +1,28 @@
+package com.vhcamposq.projurisprocessesmanager.entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Lawyer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int experienceYears;
+
+    @Column(nullable = false, unique = true)
+    private String oabNumber;
+
+}
---
+++ src/main/java/com/vhcamposq/projurisprocessesmanager/entity/Processes.java
@@ -0,0 +1,31 @@
+package com.vhcamposq.projurisprocessesmanager.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Processes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private String caseNumber;
+
+    @Column(nullable = false)
+    private String status;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+    @JoinColumn(name = "lawyer_id")
+    private Lawyer lawyer;
+}
