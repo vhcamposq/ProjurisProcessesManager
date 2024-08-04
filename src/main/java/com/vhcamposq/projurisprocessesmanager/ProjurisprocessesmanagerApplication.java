35921579dc11defcb884afcc6d0dfdb179cc9a29
Vinicius Campos <vhcamposquadros@hotmail.com>
2024-08-04 13:47:41 -0300

    Subindo base criada no spring initializr

---
+++ src/main/java/com/vhcamposq/projurisprocessesmanager/ProjurisprocessesmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.vhcamposq.projurisprocessesmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjurisprocessesmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjurisprocessesmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vhcamposq/projurisprocessesmanager/ProjurisprocessesmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vhcamposq.projurisprocessesmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjurisprocessesmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
