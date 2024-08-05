e668cd5b633526de821f93c838de80523917b72e
Vinicius Campos <vhcamposquadros@hotmail.com>
2024-08-05 10:24:08 -0300

    Adicionando comentários ao Services.

--- src/main/java/com/vhcamposq/projurisprocessesmanager/service/ProcessesService.java
+++ src/main/java/com/vhcamposq/projurisprocessesmanager/service/ProcessesService.java
@@ -11,6 +11,9 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * Serviço para operações CRUD de processos.
+ */
 @Service
 public class ProcessesService {
 
@@ -23,6 +26,13 @@ public class ProcessesService {
         this.processesRepository = processesRepository;
     }
 
+
+    /**
+     * Cria um novo processo.
+     *
+     * @param processesDTO DTO do processo a ser criado.
+     * @return Mensagem de resposta com o ID do processo criado.
+     */
     public MessageResponseDTO create(ProcessesDTO processesDTO) {
         Processes saveProcesses = processesMapper.toModel(processesDTO);
 
@@ -32,12 +42,23 @@ public class ProcessesService {
                 .build();
     }
 
+    /**
+     * Busca um processo pelo ID.
+     *
+     * @param id ID do processo a ser buscado.
+     * @return DTO do processo encontrado.
+     */
     public ProcessesDTO findById(Long id) {
         Processes processes = processesRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Processes not found"));
         return processesMapper.toDTO(processes);
     }
 
+    /**
+     * Busca todos os processos.
+     *
+     * @return Lista de DTOs de todos os processos.
+     */
     public List<ProcessesDTO> findAll() {
         return processesRepository.findAll()
                 .stream()
@@ -45,6 +66,13 @@ public class ProcessesService {
                 .collect(Collectors.toList());
     }
 
+    /**
+     * Atualiza um processo existente.
+     *
+     * @param id ID do processo a ser atualizado.
+     * @param processesDTO DTO do processo com os novos dados.
+     * @return Mensagem de resposta com o ID do processo atualizado.
+     */
     public MessageResponseDTO update(Long id, ProcessesDTO processesDTO) {
         processesRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Processes not found"));
@@ -54,6 +82,11 @@ public class ProcessesService {
         return createMessageResponse(updatedProcesses.getId(), "Processes updated with ID ");
     }
 
+    /**
+     * Deleta um processo pelo ID.
+     *
+     * @param id ID do processo a ser deletado.
+     */
     public void delete(Long id) {
         processesRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Processes not found"));
